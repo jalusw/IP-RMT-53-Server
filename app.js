5656ebc04f41aa6fd5bcd2e4546c1821921420ed
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-09-30 22:24:50 +0800

    feat: allow cors

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require("path");
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
