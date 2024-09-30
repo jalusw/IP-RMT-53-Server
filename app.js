b53baea37e46d1b1bb7140dc68785026deedd732
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-09-30 22:22:59 +0800

    feat: middlewares setup   - json   - url decoder   - morgan logger

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan("combined", {
+    stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
+      flags: "a",
+    }),
+  })
+);
 
 module.exports = app;
--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const config = {
+  env: process.env.NODE_ENV || "development",
   host: process.env.APP_HOST || "localhost",
   port: process.env.PORT || 3000,
 };
