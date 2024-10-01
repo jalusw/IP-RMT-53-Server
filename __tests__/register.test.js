ec7b051ca862b39fe457d6b9f98ea2394b28c961
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-01 08:06:30 +0800

    test: add register test

---
+++ __tests__/register.test.js
@@ -0,0 +1,64 @@
+const supertest = require("supertest");
+const request = supertest(require("../app"));
+
+const db = require("../database/models");
+
+describe("Feature - Register", () => {
+  afterAll(async () => {
+    await db.User.destroy({
+      truncate: true,
+      restartIdentity: true,
+      cascade: true,
+    });
+    await db.sequelize.close();
+  });
+
+  it("Should register a user", async () => {
+    const response = await request.post("/auth/register").send({
+      username: "test",
+      email: "test@mail.com",
+      password: "password",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe("User created");
+  });
+
+  it("Should not register a user with an existing email", async () => {
+    const response = await request.post("/auth/register").send({
+      username: "test",
+      email: "test@mail.com",
+      password: "password",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("message", "Bad Request");
+  });
+
+  it("Should not register a user with an invalid email", async () => {
+    const response = await request.post("/auth/register").send({
+      username: "test",
+      email: "testmail.com",
+      password: "password",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("message", "Bad Request");
+  });
+
+  it("Should not register a user with an invalid password", async () => {
+    const response = await request.post("/auth/register").send({
+      username: "test",
+      email: "test@mail.com",
+      password: "",
+    });
+  });
+
+  it("Should not register a user with an invalid username", async () => {
+    const response = await request.post("/auth/register").send({
+      username: "",
+      email: "test@mail.com",
+      password: "password",
+    });
+  });
+});
