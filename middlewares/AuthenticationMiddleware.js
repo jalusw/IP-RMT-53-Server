c7f7e45ecc734eea6a977888379ff6a99d8eca6d
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-01 14:52:34 +0800

    feat(auth): add authentication middleware

--- middlewares/AuthenticationMiddleware.js
+++ middlewares/AuthenticationMiddleware.js
@@ -1,16 +1,39 @@
 const createHttpError = require("http-errors");
+const AuthHelper = require("../helpers/AuthHelper");
+const { User } = require("../database/models");
 
 class AuthenticationMiddleware {
-  static handle(req, res, next) {
-    if (!req.headers.authorization) {
-      next(
-        createHttpError(401, "Invalid token", {
+  static async handle(req, res, next) {
+    try {
+      if (!req.headers.authorization) {
+        throw createHttpError(401, "Invalid token", {
           message: "access token is required",
-        })
-      );
-    }
+        });
+      }
 
-    next();
+      const [, token] = req.headers.authorization.split(" ");
+
+      const decoded = AuthHelper.verifyAccessToken(token);
+
+      const user = await User.findByPk(decoded.id);
+
+      if (!user) {
+        throw createHttpError(401, "Unauthorized", {
+          message: "Invalid token",
+        });
+      }
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      if (error.name == "JsonWebTokenError") {
+        error = createHttpError(401, "Unauthorized", {
+          message: "Invalid token",
+        });
+      }
+      next(error);
+    }
   }
 }
 
