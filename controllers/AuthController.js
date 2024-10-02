db81f8b4f920d2571e3dbc0fbd25e324973b5c25
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-02 13:04:25 +0800

    feat: add profile setting api

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -29,6 +29,9 @@ class AuthController {
   static async register(req, res, next) {
     try {
       const user = await User.create({
+        avatar:
+          req.body?.avatar ||
+          `https://ui-avatars.com/api/?name={${req.body.username}}`,
         username: req.body.username,
         email: req.body.email,
         password: HashHelper.bcrypt(req.body.password),
@@ -41,6 +44,68 @@ class AuthController {
             username: user.username,
             email: user.email,
             avatar: user.avatar,
+          },
+        },
+      });
+    } catch (error) {
+      if (
+        error.name === "SequelizeUniqueConstraintError" ||
+        error.name === "SequelizeValidationError"
+      ) {
+        error = createHttpError(400, "Bad Request", {
+          errors: error.errors.map((err) => ({
+            field: err.path,
+            message: err.message,
+          })),
+        });
+      }
+      return next(error);
+    }
+  }
+
+  static async profile(req, res, next) {
+    try {
+      return res.status(200).json({
+        message: "User found",
+        data: {
+          user: {
+            username: req.user.username,
+            email: req.user.email,
+            avatar: req.user.avatar,
+            createdAt: req.user.createdAt,
+            updatedAt: req.user.updatedAt,
+          },
+        },
+      });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  static async update(req, res, next) {
+    try {
+      const user = await User.findByPk(req.user.id);
+
+      if (!user) {
+        throw createHttpError(404, "User not found");
+      }
+
+      await user.update({
+        username: req.body.username || user.username,
+        avatar:
+          req.body.avatar ||
+          (req.body.username != user.username &&
+            `https://ui-avatars.com/api/?name=${req.body.username}`) ||
+          user.avatar,
+      });
+
+      return res.status(200).json({
+        message: "User updated",
+        data: {
+          user: {
+            avatar: user.avatar,
+            username: user.username,
+            email: user.email,
           },
         },
       });
--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const AuthController = require("../../controllers/AuthController");
+const AuthenticationMiddleware = require("../../middlewares/AuthenticationMiddleware");
 
 router.post("/login", require("../../requests/login"), AuthController.login);
 
@@ -9,5 +10,9 @@ router.post(
   require("../../requests/register"),
   AuthController.register
 );
+
+router.use("/profile", AuthenticationMiddleware.handle);
+router.get("/profile", AuthController.profile);
+router.put("/profile", AuthController.update);
 
 module.exports = router;
